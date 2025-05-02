57755bf178b0f5b01534e29cb691f9239bc41126
michael_ <kaliyuga02.02.1993@gmail.com>
2025-05-02 18:05:53 +0300

    доработал функкцию calculate_tax и её тесты

--- src/TDD.py
+++ src/TDD.py
@@ -1,5 +1,12 @@
-def calculate_tax(price: float, tax_rate: float) -> float:
+from typing import Union
+
+
+def calculate_tax(price: Union[float, int], tax_rate: Union[float, int], *, discount: Union[float, int]=0, round_symbol=2) -> Union[float, int]:
     """вычисляет стоимость товара с учетом налога и возвращать результат"""
+
+    if not isinstance(price, (float, int)) or not isinstance(tax_rate, (float, int)) or not isinstance(discount, (float, int)):
+        raise TypeError('Неверный тип данных аргумента')
+
     if price <= 0:
         raise ValueError('Неверная цена')
 
@@ -7,4 +14,4 @@ def calculate_tax(price: float, tax_rate: float) -> float:
         raise ValueError('Неверный налоговый процент')
 
     tax = price * tax_rate / 100
-    return price + tax
+    return round((price + tax) * (1 - discount / 100), round_symbol)
--- tests/test_TDD.py
+++ tests/test_TDD.py
@@ -4,7 +4,14 @@ from src.TDD import calculate_tax
 
 
 def test_calculated_tax():
-    assert calculate_tax(20.0, 10) == 22.0
+    assert calculate_tax(20.0, 10.0, discount=10) == 19.8
+
+
+def test_calculated_tax_type_error():
+    with pytest.raises(TypeError) as exc_info:
+        calculate_tax('20.0', '10.3')
+
+    assert str(exc_info.value) == 'Неверный тип данных аргумента'
 
 
 def test_calculated_price_error():
@@ -19,3 +26,7 @@ def test_calculated_tax_error():
         calculate_tax(50, -10)
 
     assert str(exc_info.value) == 'Неверный налоговый процент'
+
+
+def test_calculated_round():
+    assert calculate_tax(20.0, 10.0, discount=13.17, round_symbol = 3) == 19.103
